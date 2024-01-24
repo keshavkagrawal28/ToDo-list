e6737a4053d1fa75086ee50c9210405b8b0b0619
keshavkagrawal28 <keshavkagrawal28@gmail.com>
2024-01-24 13:23:37 +0530

    Add TODO via form

--- src/app/todos/todos.component.ts
+++ src/app/todos/todos.component.ts
@@ -2,20 +2,32 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { Todo } from '../shared/todo.model';
 import { DataService } from '../shared/data.service';
+import { FormsModule, NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-todos',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './todos.component.html',
   styleUrl: './todos.component.scss',
 })
 export class TodosComponent implements OnInit {
   todos!: Todo[];
+  showValidationErrors: boolean = false;
 
   constructor(private dataService: DataService) {}
 
   ngOnInit(): void {
     this.todos = this.dataService.getAllTodos();
+  }
+
+  onFormSubmit(form: NgForm) {
+    if (form.invalid) {
+      this.showValidationErrors = true;
+      return;
+    }
+    let todoText = form.value['text'];
+    this.dataService.addTodo(new Todo(todoText));
+    form.reset();
   }
 }
